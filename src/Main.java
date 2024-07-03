ba37be1f9be7a8237b6a40174a73ba956376ed51
MaggyDocente <magali.sanchez@bue.edu.ar>
2024-07-03 09:54:26 -0300

    Se agregaron metodos de la clase Libro, y su constructor

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,8 @@
-import clases.Biblioteca;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String nombre;
 
-        nombre = "Macarena";
-
-        System.out.printf(nombre);
-
-        //Firma de una funcion
-        //alcance (publico, privado), lo que retorna (void, String,etc)
-        //nombre de la funcion, y luedo sus parametros que van entre
-        //parentesis, finalmente las llaves
-
-
-        Biblioteca biblioteca1 = new Biblioteca();
     }
+
 }
--- src/clases/Libro.java
+++ src/clases/Libro.java
@@ -1,5 +1,15 @@
 package clases;
 
 public class Libro {
+   private Integer id;
+   private String titulo;
+   private String autor;
+   private Integer cantPag;
 
+   public Libro(String titulo, String autor, Integer cantPag){
+       this.id = 0;
+       this.titulo = titulo;
+       this.autor = autor;
+       this.cantPag = cantPag;
+   }
 }
